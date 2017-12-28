340c67b4c4c7f7c57af1e903fa66d390b17970f8
jaimedom <jaimedominguezdp@gmail.com>
2017-12-28 10:23:03 +0000

    Fix problem

--- app.py
+++ app.py
@@ -12,47 +12,47 @@ price_options = ['Close', 'Adjusted close', 'Open', 'Adjusted Open']
 @app.route('/')
 def index():
     
-  code = request.args.get('code')
-  if code == None:
-    code = 'GOOG'
+    code = request.args.get('code')
+    if code == None:
+        code = 'GOOG'
         
-  price = request.args.get('price_option')
-  if price == None:
-    price = 'Open'
+    price = request.args.get('price_option')
+    if price == None:
+        price = 'Open'
         
-  price_search = {'Close': 'close',
-             'Adjusted close': 'adj_close',
-             'Open': 'open',
-             'Adjusted Open': 'adj_open'
-            }[price]
+    price_search = {'Close': 'close',
+                    'Adjusted close': 'adj_close',
+                    'Open': 'open',
+                    'Adjusted Open': 'adj_open'
+                   }[price]
         
-  api_key = '6LqqHDqZYZX4TaPdPnKR'
+    api_key = '6LqqHDqZYZX4TaPdPnKR'
     
-  quandl = 'https://www.quandl.com/api/v3/datatables/WIKI/PRICES.json?date.gte=20170101&date.lt=20171231'
-  quandl += '&ticker=' + code
-  quandl += '&qopts.columns=date,' + price_search
-  quandl += '&api_key=' + api_key
+    quandl = 'https://www.quandl.com/api/v3/datatables/WIKI/PRICES.json?date.gte=20170101&date.lt=20171231'
+    quandl += '&ticker=' + code
+    quandl += '&qopts.columns=date,' + price_search
+    quandl += '&api_key=' + api_key
     
-  json_file = requests.get(quandl).json()
-  df = pd.DataFrame(json_file['datatable']['data'])
-  df[0] = pd.DatetimeIndex(df[0])
-  source = ColumnDataSource(data = {'0':df[0],'1':df[1]})
+    json_file = requests.get(quandl).json()
+    df = pd.DataFrame(json_file['datatable']['data'])
+    df[0] = pd.DatetimeIndex(df[0])
+    source = ColumnDataSource(data = {'0':df[0],'1':df[1]})
 
-  p = figure(title = 'Price for '+ code,
+    p = figure(title = 'Price for '+ code,
                x_axis_label = 'Date',
                y_axis_label = 'Price ($)',
                x_axis_type = 'datetime'
-               )
+              )
 
-  p.y_range.start = min(df[1])
-  p.y_range.end = max(df[1])
+    p.y_range.start = min(df[1])
+    p.y_range.end = max(df[1])
 
-  p.line(x='0',y='1',source=source,color='blue',legend=None)
-  script, div = components(p)
+    p.line(x='0',y='1',source=source,color='blue',legend=None)
+    script, div = components(p)
     
-  return render_template("plot.html", script=script, div=div,
-		price_options=price_options, current_code=code, 
-         current_selected_price=price)
+    return render_template("plot.html", script=script, div=div,
+                          price_options=price_options, current_code=code, 
+                          current_selected_price=price)
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0',port=33507,debug=True)
+    app.run(host='0.0.0.0',port=33507,debug=True)
