3ec67fa27abb958f30babb5cb160292f75fe31ce
jaimedom <jaimedominguezdp@gmail.com>
2017-12-28 18:03:01 +0000

    Locally working version

--- app.py
+++ app.py
@@ -9,49 +9,48 @@ app = Flask(__name__)
 
 price_options = ['Close', 'Adjusted close', 'Open', 'Adjusted Open']
 
-@app.route('/', methods=['GET','POST'])
+@app.route('/')
 def index():
-    if request.method == 'POST':
     
-        code = request.args.get('code')
-        if code == None:
-            code = 'GOOG'
+    code = request.args.get('code')
+    if code == None:
+        code = 'GOOG'
         
-        price = request.args.get('price_option')
-        if price == None:
-            price = 'Open'
+    price = request.args.get('price_option')
+    if price == None:
+        price = 'Open'
+    
+    price_search = {'Close': 'close',
+                    'Adjusted close': 'adj_close',
+                    'Open': 'open',
+                    'Adjusted Open': 'adj_open'
+                   }[price]
         
-        price_search = {'Close': 'close',
-                 'Adjusted close': 'adj_close',
-                 'Open': 'open',
-                 'Adjusted Open': 'adj_open'
-                }[price]
-        
-        api_key = '6LqqHDqZYZX4TaPdPnKR'
+    api_key = '6LqqHDqZYZX4TaPdPnKR'
     
-        quandl = 'https://www.quandl.com/api/v3/datatables/WIKI/PRICES.json?date.gte=20170101&date.lt=20171231'
-        quandl += '&ticker=' + code
-        quandl += '&qopts.columns=date,' + price_search
-        quandl += '&api_key=' + api_key
+    quandl = 'https://www.quandl.com/api/v3/datatables/WIKI/PRICES.json?date.gte=20170101&date.lt=20171231'
+    quandl += '&ticker=' + code
+    quandl += '&qopts.columns=date,' + price_search
+    quandl += '&api_key=' + api_key
     
-        quandl_data = requests.get(quandl)
-        stock_load = json.loads(quandl_data.content) 
-        df = pd.DataFrame(stock_load['datatable']['data'])
-        df[0] = pd.DatetimeIndex(df[0])
+    quandl_data = requests.get(quandl)
+    stock_load = json.loads(quandl_data.content) 
+    df = pd.DataFrame(stock_load['datatable']['data'])
+    df[0] = pd.DatetimeIndex(df[0])
 
-        p = figure(title = 'Price for '+ code,
-                   x_axis_label = 'Date',
-                   y_axis_label = 'Price ($)',
-                   x_axis_type = 'datetime'
-                   )
+    p = figure(title = 'Price for '+ code,
+               x_axis_label = 'Date',
+               y_axis_label = 'Price ($)',
+               x_axis_type = 'datetime'
+               )
 
-        p.y_range.start = min(df[1])
-        p.y_range.end = max(df[1])
+    p.y_range.start = min(df[1])
+    p.y_range.end = max(df[1])
 
-        p.line(x=df[0],y=df[1],color='blue',legend=None)
-        script, div = components(p)
+    p.line(x=df[0],y=df[1],color='blue',legend=None)
+    script, div = components(p)
     
-        return render_template("plot.html", script=script, div=div,
+    return render_template("plot.html", script=script, div=div,
             price_options=price_options, current_code=code, 
             current_selected_price=price)
     
